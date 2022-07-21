44cb241848c6c5bb6998c1933fec1eebaa2643fa
An Bui <quangan186@gmail.com>
2022-07-21 10:47:46 +0700

    setup ios project

---
+++ memeland/memeland/memelandApp.swift
@@ -0,0 +1,17 @@
+//
+//  memelandApp.swift
+//  memeland
+//
+//  Created by Bui Quang An on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct memelandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
